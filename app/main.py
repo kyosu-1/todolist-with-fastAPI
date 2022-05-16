83ff337fa87e5f29d3cc87088a6d9869dba8ee1a
kyosu-1 <sho013039@gmail.com>
2022-05-17 07:38:52 +0900

    add sample endpoint

--- app/main.py
+++ app/main.py
@@ -5,11 +5,16 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/api/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/api/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/api/sample")
+def return_sample():
+    return {"sample": "tmp"}
